1db0c3f3ddbcd4e07b7e7be40cf4c1b8526441bc
mitikamishra05 <155060558+mitikamishra05@users.noreply.github.com>
2024-01-08 22:09:47 +0530

    day01.py

--- Unit 1/Day01.py
+++ Unit 1/Day01.py
@@ -1,18 +1,17 @@
-import turtle as tur 
-import colorsys as cs
-tur.setup(800,800)
-tur.speed(0)
-tur.tracer(10)
-tur.width(2)
-tur.bgcolor("black")
-for j in range(25):
-    for i in range(15):
-        tur.color(cs.hsv_to_rgb(i/15,j/25,1))
-        tur.right(90)
-        tur.circle(200-j*4,90)
-        tur.left(90)
-        tur.circle(200-j*4,90)
-        tur.right(180)
-        tur.circle(50,24)
-tur.hideturtle()
-tur.done()
+import turtle as t
+t.pen()
+t.bgcolor("black")
+colors=["red","green","yellow","blue","gray",
+"purple","aqua","brown",]
+name=t.textinput("Enter your name",
+"Enter your name")
+s=int(t.numinput("number of sides",
+"how many sides you want(1-20)",10,1,20))
+for i in range(100):
+    t.pencolor(colors[i%s%10])
+    t.penup()
+    t.forwad(i*5)
+    t.pendown()
+    t.write(name,font=("",
+    int((i*2+4)/4),"bold"))
+    t.left(360/s+4)
