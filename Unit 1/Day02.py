8e1869bffa1a37f069b5c63418383552c52cf05f
mitikamishra05 <155060558+mitikamishra05@users.noreply.github.com>
2024-01-08 21:42:08 +0530

    day02.py

--- Unit 1/Day02.py
+++ Unit 1/Day02.py
@@ -1,18 +1,16 @@
-import turtle 
-import colorsys
-t = turtle.Turtle()
-s = turtle.Screen() .bgcolor('black')
-t.speed(0)
-n = 70
-h = 0
-for i in range (360):
-    c = colorsys.hsv_to_rgb(h , 1, 0.8)
-    h+= 1/n
-    t.color(c)
-    t.left(1)
-    t.fd(1)
-    for j in range (2):
-    t.left (2)
-    t.circle (100)
-tur.hideturtle()
-tur.done()
+import math
+from turtle import *
+def hearta(k):
+    return 15*math.sin(k)**3
+def heartb(k):
+    return 12*math.cos(k)-5*\
+           math.cos(2*k)-2*\
+           math.cos(3*k)-\
+           math.cos(4*k)
+speed(1000)
+bgcolor("black")
+for i in range(6000):
+    goto(hearta(i)*20,heartb(i)*20)
+    for j in range(5):
+        color("#f73487")
+        goto(0,0)
