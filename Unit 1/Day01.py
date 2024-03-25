6293e6e638901e956072b01af3c013c95042e75f
mitikamishra05 <155060558+mitikamishra05@users.noreply.github.com>
2024-03-25 22:57:39 +0530

    day01

--- Unit 1/Day01.py
+++ Unit 1/Day01.py
@@ -1,17 +1,5 @@
-import turtle as t
-t.pen()
-t.bgcolor("black")
-colors=["red","green","yellow","blue","gray",
-"purple","aqua","brown",]
-name=t.textinput("Enter your name",
-"Enter your name")
-s=int(t.numinput("number of sides",
-"how many sides you want(1-20)",10,1,20))
-for i in range(100):
-    t.pencolor(colors[i%s%10])
-    t.penup()
-    t.forwad(i*5)
-    t.pendown()
-    t.write(name,font=("",
-    int((i*2+4)/4),"bold"))
-    t.left(360/s+4)
+int(input("4"))
+int(input("3"))
+if (4>3)
+    print("4" is larger)
+else print("3" is larger)
